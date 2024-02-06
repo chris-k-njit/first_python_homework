b55c7ba119add3ecd9d8ac25d32dca752bdaf431
ck378 <ck378@njit.edu>
2024-02-05 20:56:26 -0500

    added add and subtract from Professor Williams, also added multiple and divide to see how this way work when testing things out.

---
+++ tests/__init__.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+# Added some of my own custom code to test this as well
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
