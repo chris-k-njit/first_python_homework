9677b7028c09435d7c448718413229d7e54f02a8
ck378 <ck378@njit.edu>
2024-02-06 23:39:18 -0500

    added white space, added more tests.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,19 @@
 # Add and subtract from Professor Williams
 # Added multiply and divide as additional test for this assignment.
-from calculator import add, subtract 
-# multiply, divide -- stopped due to type error
+from calculator import add, subtract, multiply, divide, exponential
 
 def test_addition(): # Testing addition
-    assert add(2,2) == 4
+    assert add(4,4) == 8
 
 def test_subtraction(): # Test subtraction
-    assert subtract(2,2) == 0
+    assert subtract(7,1) == 6
 
-"""  Type error, resolving later.
-def test_multiplication(): 
+def test_multiplication(): # test multiplication
     assert multiply(1*2) == 2
 
-def test_division():
+def test_division(): # test division
     assert divide(4/2) == 2
-    """
+
+def test_exponents(): ## test exponent
+    assert exponential(2**2) == 4
+    
